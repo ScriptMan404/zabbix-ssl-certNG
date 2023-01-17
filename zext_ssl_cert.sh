2612643981536f66bea3b281dc8252a33de5cf04
ScriptMan404 <45128030+ScriptMan404@users.noreply.github.com>
2023-01-17 18:24:56 +0100

    Update zext_ssl_cert.sh
    Use timeout variable

--- zext_ssl_cert.sh
+++ zext_ssl_cert.sh
@@ -9,25 +9,26 @@
 f=$1
 sni=$2
 port=$3
+timeout=2.5
 
 case $f in
 -d)
-end_date=$(timeout 0.3 openssl s_client -connect $2:$3 -crlf -servername $2 2> /dev/null | openssl x509 -noout -enddate | sed -e "s/^notAfter=//")
+end_date=$(timeout $timeout openssl s_client -connect $2:$3 -crlf -servername $2 2> /dev/null | openssl x509 -noout -enddate | sed -e "s/^notAfter=//")
 echo $((($(date +%s --date "$end_date") - $(date +%s)) / 86400))
 ;;
 
 -i)
-echo $(timeout 0.3 openssl s_client -connect $2:$3 -crlf -servername $2 2> /dev/null | openssl x509 -noout -issuer | sed -e "s/.*CN = *//")
+echo $(timeout $timeout openssl s_client -connect $2:$3 -crlf -servername $2 2> /dev/null | openssl x509 -noout -issuer | sed -e "s/.*CN = *//")
 ;;
 
 -s)
-echo $(timeout 0.3 openssl s_client -connect $2:$3 -crlf -servername $2 2> /dev/null | openssl x509 -noout -subject | sed -e "s/.*CN = *//")
+echo $(timeout $timeout openssl s_client -connect $2:$3 -crlf -servername $2 2> /dev/null | openssl x509 -noout -subject | sed -e "s/.*CN = *//")
 ;;
 
 *)
-echo "usage: $0 [-i|-d|-s] sni port"
-echo "    -i Show Issuer"
-echo "    -d Show valid days remaining"
-echo "    -s Show Subject"
+echo "usage : $0 [-i|-d|-s] sni port"
+echo "        -i Show Issuer"
+echo "        -d Show valid days remaining"
+echo "        -s Show Subject"
 ;;
 esac
